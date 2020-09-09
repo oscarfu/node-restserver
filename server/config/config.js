3bfabfe3c3fe26f2fbe514cad7c0244af84bce1e
oscar <orfcomputer@gmail.com>
2020-09-09 15:55:53 -0500

    Rest server -usuario

--- server/config/config.js
+++ server/config/config.js
@@ -1,1 +1,17 @@
 process.env.PORT = process.env.PORT || 3000;
+
+
+//mongodb+srv://oscarfuel:4yTkADikr0x22V5S@cluster0.d5jxo.mongodb.net/cafe
+//mongodb://localhost:27017/cafe
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+let urlDB;
+
+// if (process.env.NODE_ENV === 'dev') {
+//     urlDB = 'mongodb://localhost:27017/cafe';
+// } else {
+urlDB = 'mongodb+srv://oscarfuel:4yTkADikr0x22V5S@cluster0.d5jxo.mongodb.net/cafe';
+// }
+
+process.env.URLDB = urlDB;
--- server/server.js
+++ server/server.js
@@ -1,44 +1,35 @@
 require('./config/config')
 
+
 const express = require('express');
 const app = express();
+
+const mongoose = require('mongoose');
+
 const bodyParser = require('body-parser');
 
+
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // parse application/json
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
-app.get('/usuario', function(req, res) {
-    res.json('get usuario');
-})
+app.use(require('./routes/usuario'));
 
-app.post('/usuario', function(req, res) {
+mongoose.connect(process.env.URLDB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true
+});
 
-    let body = req.body;
-
-    if (body.nombre === undefined) {
-        res.status(400).json({
-            ok: false,
-            mensaje: 'El nombre es necesario'
-        })
-    } else {
-
-        res.json({ persona: body });
-    }
-
-})
-
-app.put('/usuario/:id', function(req, res) {
-
-    let id = req.params.id;
-    res.json({ id });
-})
-
-app.delete('/usuario', function(req, res) {
-    res.json('delete usuario');
-})
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+    // we're connected!
+    console.log('Conectado!')
+});
 
 app.listen(process.env.PORT, () => {
     console.log('Escuchando puerto: ', process.env.PORT)
