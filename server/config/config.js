4b330441f2dde5551725e62be9df4b89dd013a0d
oscar <orfcomputer@gmail.com>
2020-09-09 16:31:30 -0500

    agregado entorno

--- server/config/config.js
+++ server/config/config.js
@@ -1,8 +1,7 @@
 process.env.PORT = process.env.PORT || 3000;
 
 
-//mongodb+srv://oscarfuel:4yTkADikr0x22V5S@cluster0.d5jxo.mongodb.net/cafe
-//mongodb://localhost:27017/cafe
+
 
 process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
@@ -11,7 +10,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://oscarfuel:4yTkADikr0x22V5S@cluster0.d5jxo.mongodb.net/cafe';
+    urlDB = process.env.MOmongo_uri;
 }
 
 process.env.URLDB = urlDB;
